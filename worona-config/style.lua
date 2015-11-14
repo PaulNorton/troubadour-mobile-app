9d2c633d3211c11c6a3b556cda30e6c5dbb02ed3
Paul Norton <paul@nortoncrew.com>
2015-11-14 15:51:18 -0500

    Change nabber color to forest green

--- worona-config/style.lua
+++ worona-config/style.lua
@@ -1,7 +1,7 @@
 return {
 	-- Navbar --
-	navbar_main_color   = { 0.203125, 0.59375, 0.85546875, 1 },  --. Sets navbar main color
-	navbar_stroke_color = { 0.16, 0.5, 0.72, 1 },				 --. Sets navbar stroke color
+	navbar_main_color   = { 0.1490196078, 0.4156862745, 0.1803921569, 1 },  --. Sets navbar main color
+	navbar_stroke_color = { 0.08235294118, 0.1921568627, 0.1529411765, 1 },				 --. Sets navbar stroke color
 	navbar_stroke_height = 4,									 --. Sets navbar stroke width
 	navbar_text_color   = { 1, 1, 1, 1 },						 --. Sets navbar text color
 
