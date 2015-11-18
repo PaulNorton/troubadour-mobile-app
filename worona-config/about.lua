869e5c88390129a7929fdacab8f14a040102bc35
Paul Norton <paul@nortoncrew.com>
2015-11-18 16:57:21 -0500

    Shorten about section

--- worona-config/about.lua
+++ worona-config/about.lua
@@ -1,9 +1,14 @@
 local worona = require "worona"
 
-worona.app_about_title       = "About The Troubadour"						-- Enter the title of the about section.
-worona.app_about_description = "The Troubadour is the student newspaper serving Franciscan University in Steubenville, Ohio, a private liberal arts college with a student body of 2,500.\n\nThe Troubadour does not reflect the views of the university administration, but it does try to conform to Catholic values and stay within the vision and mission statement of the university.\n\nUnsigned editorials represent the opinion of the majority of the editorial board. Letters will not be published unless their authorship can be verified by phone. Commentaries, opinion articles, letters and editorial columns represent the views of their authors. All submissions are subject to editing for length and content at the discretion of The Troubadour staff.\n\nThe deadline for entries each week is Saturday at midnight. They can be mailed, e-mailed or hand-delivered to The Troubadour office.\n\nThe Troubadour\nFranciscan University of Steubenville\n1235 University Blvd.\nSteubenville, OH 43952\n\n(740) 284-5014"
+worona.app_about_title       = "About The Troubadour"				-- Enter the title of the about section.
+worona.app_about_description =	"The Troubadour is the student newspaper serving Franciscan University in Steubenville, Ohio, a private liberal arts college with a student body of 2,500.\n\n" .. 
+								"The Troubadour does not reflect the views of the university administration, but it does try to conform to Catholic values and stay within the vision and mission statement of the university.\n\n\n" .. 
+								"The Troubadour\n" .. 
+								"www.troubonline.com\n" .. 
+								"(740) 284-5014\n\n\n" .. 
+								"Developed by Paul Norton, Henry Hudson, Blaise Lamatrice, and Connor Gaede"
 worona.your_logo             = false 								-- Activate or deactivate your logo with (true or false)
 worona.your_logo_icon        = "worona-config/images/your_logo.png"	-- Path to the Custom Logo
 worona.your_logo_width       = 250									-- Logo width
 worona.your_logo_height      = 100									-- Logo height
-worona.your_logo_link        = "http://www.worona.org"  			-- Logo link
+worona.your_logo_link        = "http://www.troubonline.com"  		-- Logo link
